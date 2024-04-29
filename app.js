a4101e6ff08700d91c641d8ad11b2871a564709c
Arya Patel <107814928+aryapatxl@users.noreply.github.com>
2024-04-29 15:21:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var ps3 = require('/Users/arya/hw2-cs412/ps3.js');
+var ps3 = require('./ps3');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use('/ps3', ps3);
 
 app.set('view engine', 'pug');
